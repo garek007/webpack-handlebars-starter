ed6f093d95d84f0ed938188f32de6802e8c6286f
Alachniewicz <stan.alachniewicz@labcorp.com>
2026-03-11 11:04:49 -0700

    added slds

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import template from './index.hbs';
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css';
 
 
 
